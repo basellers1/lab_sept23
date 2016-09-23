1e347746e578275450de74bc53928b2c2b32ca4e
Britt Anne Sellers <basellers1@yahoo.com>
2016-09-23 14:15:51 -0700

    changed question2

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a,b){
-var sum = a + b;
+function sum(a,b){ // eslint-disable-line
+  var sum = a + b;
   var message = 'The sum of ' + a + ' and ' + b + ' is ' + sum + '.';
-var output = [sum, message];
-return output;
+  var output = [sum, message];
+  return output;
 }
 
 // Here is the test for sum(); uncomment it to run it
@@ -27,13 +27,13 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiply(a,b){ //eslint-disable-line
   var multiply = a * b;
-  var message = ('The result of ' + a * b + ' is ' + multiply + '.');
+  var message = 'The product of ' + a + ' and ' + b + ' is ' + multiply + '.';
   var output = [multiply, message];
   return output;
 }
 
 // Here is the test for multiply(); uncomment it to run it
-multiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
